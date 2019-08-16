0e90675f34aa232e522088d9eaad6b0c2e995d08
Matt Williams <matt.seongjun@gmail.com>
2019-08-16 09:25:10 -0400

    Done"

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+
+def sum_of_squares(limit)
+    i = 0
+    squares = []
+    while i <= limit
+        squares << i ** 2
+        i += 1
+    end
+    squares.inject{ |sum, n| n + sum }
+end
+
+def square_of_sums(limit)
+    array = (1..limit).to_a 
+    sum = array.inject{ |sum , n| n + sum }
+    sum ** 2
+end
+
+def difference(limit)
+    square_of_sums(limit) - sum_of_squares(limit)
+end
+
+puts sum_of_squares(10)
+puts square_of_sums(10)
+puts difference(10)
+puts difference(100)
